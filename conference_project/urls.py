c44d62392b091a6876be0e6fb41ed12d6e560859
Pelin Mutanguha <pelmut2000@gmail.com>
2023-06-14 19:45:32 +0200

    add urls and some dummy templates

--- conference_project/urls.py
+++ conference_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import views
 
 urlpatterns = [
+    # www.conference.rw/admin/
     path('admin/', admin.site.urls),
+
+
+    path('', include('core.urls'))
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    data = ['django', 'laravel', 'asp.net core', 'express']
+    return render(request, 'home.html', {'data': data})
+
+
+def about_view(request):
+    return HttpResponse("<h1>About Page</h1><br><a href='/'>Go back to home</a>")
+
+
+def testing_stuff(request, number):
+    # DB query
+    #
+    # number = ''
+    # if id < 5:
+    #     number = 'xzy'
+    return render(request, 'testing.html', {'number': number})
