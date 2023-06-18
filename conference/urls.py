10b21f91e87b1968ec32c4237dc62b6ed44b92c8
ericndungutse <dav.ndungutse@gmail.com>
2023-06-18 23:35:13 +0200

    display conference detail

--- conference/urls.py
+++ conference/urls.py
@@ -3,5 +3,6 @@ from conference import views
 
 urlpatterns = [
     path('conferences/', views.conferences_view),
-    path('conferences/create', views.create_conference_view)
+    path('conferences/create', views.create_conference_view),
+    path('conferences/<id>', views.conference_view)
 ]
--- conference/views.py
+++ conference/views.py
@@ -1,40 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def conferences_view(request):
-    # name, dates, and location
-    conferences = [
+conferences = [
         {
+            "id": 1,
             "name": "Introduction to AI",
             "date": "2023-03-12",
             "location": "BK Arena"
         },
         {
+            "id": 2,
             "name": "Data Science in health",
             "date": "2023-12-12",
             "location": "Intare Conference Arena"
         },
         {
+            "id": 3,
             "name": "Impact of AI on employment",
             "date": "2023-04-02",
             "location": "Hotel Umubano"
         },
 
         {
+            "id": 4,
             "name": "Developing Tech Hubs in Africa",
             "date": "2024-01-01",
             "location": "Intare Conference Arena"
         },
 
         {
+            "id": 5,
             "name": "FIFA Summit 2024",
             "date": "2024-07-01",
             "location": "BK Arena"
         },
     ]
-
+def conferences_view(request):
     return render(request, 'conference/conferences.html', context={"conferences": conferences})
 
 def create_conference_view(request):
     return render(request, "conference/createConference.html")
+
+def conference_view(request,id):
+    conference = None
+    for conf in conferences:
+        if conf["id"] == int(id):
+            conference = conf
+
+    print(conference)
+    return render(request, "conference/conference.html", context={"conference": conference })
+
