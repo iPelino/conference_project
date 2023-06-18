b6a6385764d50e14cf0134df5227f1eeb0d782da
pierre311 <masengesho1976@gmail.com>
2023-06-18 22:12:10 +0300

    models.py modified

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Model(models.Model):
+    # Define fields and their types here
+    field1 = models.CharField(max_length=100)
+    field2 = models.IntegerField()
+    # ... here you add many fields as you wish
+
+    def __str__(self):
+        return self.field1 
+ # Return a string representation of the model instance
