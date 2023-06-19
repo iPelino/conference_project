1d8f3ea1d8d3adf9da765a5dfe00ea9a9390c056
irumva253 <irumvapatrick253@gmail.com>
2023-06-19 23:02:30 +0200

    display and create form

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,13 @@ from core import views
 urlpatterns = [
     # www.conference.rw
     path('', views.home_view, name='home'),
-    path('<str:number>/', views.testing_stuff, name='testing'),
+    path('conferences/', views.testing_stuff, name='testing'),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
 
     # www.conference.rw/about/
     path('about/', views.about_view, name='about'),
+    
+    # www.conference.rw/references/create/
+    path('conferences/create/', views.create_view, name='create'),
 ]
--- core/views.py
+++ core/views.py
@@ -11,10 +11,12 @@ def about_view(request):
     return HttpResponse("<h1>About Page</h1><br><a href='/'>Go back to home</a>")
 
 
-def testing_stuff(request, number):
-    # DB query
-    #
-    # number = ''
-    # if id < 5:
-    #     number = 'xzy'
-    return render(request, 'testing.html', {'number': number})
+def testing_stuff(request):
+     data = [{'conference_id':1,'conference_name':'CHOGM','date':'Mon, Jun 20, 2022 - Sun, Jun 26, 2022','location':'Kigali Convention Center'},
+            {'conference_id':2,'conference_name':'YouthConnekt Africa','date':'Oct 15, 2022','location':'BK Arena'},
+            {'conference_id':3,'conference_name':'Commonwealth of Nations','date':'Mon, Mar 13, 2023','location':'Kigali Convention Centery'},]
+     return render(request, 'testing.html', {'data': data})
+
+def create_view(request):
+    data = ['create']
+    return render(request, 'create.html', {'data': data})
