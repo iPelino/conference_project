3ee59d9983e836005dd486262c6a6bd52baeedb7
Henry Shema <84914138+shemahenry@users.noreply.github.com>
2023-06-19 23:43:11 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,13 @@ urlpatterns = [
     # www.conference.rw
     path('', views.home_view, name='home'),
     path('<str:number>/', views.testing_stuff, name='testing'),
-    path('conferences/', conference_list, name='conference_list'),
-    path('conferences/create/', views.create_conference, name='create_conference'),
-    path('conferences/<int:conference_id>/', views.conference_details, name='conference_details'),
-    path('conferences/<int:conference_id>/update/', views.update_conference, name='update_conference'),
-    path('conferences/<int:conference_id>/delete/', views.delete_conference, name='delete_conference'),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
+    path('conferences/', views.conference_list, name='conference_list'),
+    path('conferences/<int:conference_id>/', views.conference_details, name='conference_details'),
+    path('conferences/create/', views.create_conference, name='create_conference'),
+    path('conferences/<int:conference_id>/update/', views.update_conference, name='update_conference'),
+    path('conferences/<int:conference_id>/delete/', views.delete_conference, name='delete_conference'),
 
     # www.conference.rw/about/
     path('about/', views.about_view, name='about'),
