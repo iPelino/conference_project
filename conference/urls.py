2213d215222331578722dcc9a87c1e2540915f28
Gisele <giselleusanase@gmail.com>
2023-06-19 17:46:06 +0530

    all confrences lists

---
+++ conference/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from conference import views
+
+urlpatterns = [
+    path('conferences/', views.conferences_view)
+]
---
+++ conference/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+conferences = [
+        {
+            "id": 1,
+            "name": "SCA contributhon",
+            "date": "2023-03-12",
+            "location": "Raddison Blu"
+        },
+        {
+            "id": 2,
+            "name": "Women in stem",
+            "date": "2023-12-12",
+            "location": "Kigali conventionn center"
+        },
+        {
+            "id": 3,
+            "name": "Code beta solutions",
+            "date": "2023-04-02",
+            "location": "Kigali Heights"
+        },
+
+        {
+            "id": 4,
+            "name": "Africa science week",
+            "date": "2024-01-01",
+            "location": "Park inn"
+        },
+
+        {
+            "id": 5,
+            "name": "Kigali founders happy hour",
+            "date": "2024-07-01",
+            "location": "Ubumwe Hotel"
+        },
+    ]
+def conferences_view(request):
+    return render(request, 'confs.html', context={"conferences": conferences})
