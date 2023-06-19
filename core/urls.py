4424851f840101411ee82982273732990c2eac87
kagoyire Rebecca <rebeccanw386@gmail.com>
2023-06-19 13:18:39 -0700

    views added

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, re_path
 from core import views
 urlpatterns = [
-    # www.conference.rw
+     # www.conference.rw
     path('', views.home_view, name='home'),
+    path('conferences/create/',views.new_conference,name='create'),
+    path("conferences/<int:conference_id>/",views.conference_detail,name='detail'),
+    path("conferences/<int:conference_id>/update/",views.update_conference,name='update'),
+    path("conferences/<int:conference_id>/delete/",views.delete_conference,name='delete'),
+
+    path('conferences/',views.all_conference_view,name='conference'),
     path('<str:number>/', views.testing_stuff, name='testing'),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
