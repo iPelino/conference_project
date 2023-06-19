5de0eaa6a19be27885692ce45385427284e55a56
pc <sabwasabrine@gmail.com>
2023-06-19 13:57:15 -0700

    added update and delete feature

--- speakers/urls.py
+++ speakers/urls.py
@@ -3,8 +3,9 @@ from speakers import views
 
 urlpatterns = [
     path('/', views.speaker_list),
-    path('/create speaker', views.create_speaker)
-    path('<speakers_id:speakers.id>/',view.speaker_details)
-    path('<int:speaker_id>/update/', views.update_speaker, name='update_speaker')
+    path('/create speaker', views.create_speaker),
+    path('<speakers_id:speakers.id>/',view.speaker_details),
+    path('<int:speaker_id>/update/', views.update_speaker, name='update_speaker'),
+     path('<int:speaker_id>/delete/', views.delete_speaker, name='delete_speaker'),
 ]
 
