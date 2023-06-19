b7cc510985d2ea727b41ea95d50b564b4d6f0946
Jeph2001 <josephmaniragaba9@gmail.com>
2023-06-19 22:47:28 +0200

    final commit

--- speakers/views.py
+++ speakers/views.py
@@ -21,3 +21,15 @@ def speaker_details(request, speaker_id):
     speaker = {"id": speaker_id, "name": "Speaker", "bio": "Bio", "contact": "Contact"}
     return render(request, "speakers/speaker_details.html", {"speaker": speaker})
 
+
+def update_speaker(request, speaker_id):
+    # Retrieve speaker information based on speaker_id (dummy data)
+    speaker = {"id": speaker_id, "name": "Speaker", "bio": "Bio", "contact": "Contact"}
+    return render(request, "speakers/update_speaker.html", {"speaker": speaker})
+
+
+def delete_speaker(request, speaker_id):
+    return render(request, 'speakers/speaker_delete.html')
+
+
+
