b4833a81be0b2e03f27f82285f6f70866b0eff5b
pacifique151 <irakiranukapacifique@gmail.com>
2023-06-19 17:11:14 -0500

    feat: Implement Conference model for CRUD operations
    - Created Conference model with fields for name, dates, location, and topics
    - Enables CRUD functionality for managing conference information
    
    This commit introduces the Conference model in the models.py file of the conference app. The model includes fields to store conference details such as name, dates, location, and topics. These fields allow for creating, reading, updating, and deleting conference information.
    
    With the Conference model in place, the application gains the ability to perform the following CRUD operations:
    - List all conferences
    - Create a new conference
    - Retrieve details of a specific conference
    - Update information of a specific conference
    - Delete a specific conference
    
    The implementation of the Conference model lays the groundwork for handling conference management within the project.
    
    Fixes #<issue>

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Conference(models.Model):
+    name = models.CharField(max_length=100)
+    dates = models.DateField()
+    location = models.CharField(max_length=100)
+    topics = models.TextField()
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Conference
 
 def home_view(request):
     data = ['django', 'laravel', 'asp.net core', 'express']
@@ -18,3 +18,24 @@ def testing_stuff(request, number):
     # if id < 5:
     #     number = 'xzy'
     return render(request, 'testing.html', {'number': number})
+
+
+
+def conference_list(request):
+    conferences = Conference.objects.all()
+    return render(request, 'conference/conference_list.html', {'conferences': conferences})
+
+def conference_details(request, conference_id):
+    conference = Conference.objects.get(id=conference_id)
+    return render(request, 'conference/conference_details.html', {'conference': conference})
+
+def conference_create(request):
+    return render(request, 'conference/conference_create.html')
+
+def conference_update(request, conference_id):
+    conference = Conference.objects.get(id=conference_id)
+    return render(request, 'conference/conference_update.html', {'conference': conference})
+
+def conference_delete(request, conference_id):
+    conference = Conference.objects.get(id=conference_id)
+    return render(request, 'conference/conference_delete.html', {'conference': conference})
