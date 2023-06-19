8e91aa081b7c9be49a635946f336db05dd83b904
Bruno <84916998+Brunoish@users.noreply.github.com>
2023-06-19 21:22:22 +0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+
+
+
 def conference_list(request):
     conferences = [
-        {'name': 'Conference 1', 'dates': 'June 1-3, 2023', 'location': 'City A'},
-        {'name': 'Conference 2', 'dates': 'July 5-7, 2023', 'location': 'City B'},
-        {'name': 'Conference 3', 'dates': 'August 10-12, 2023', 'location': 'City C'},
-        {'name': 'Conference 4', 'dates': 'September 15-17, 2023', 'location': 'City D'},
-        {'name': 'Conference 5', 'dates': 'October 20-22, 2023', 'location': 'City E'},
+        {"id":0,'name': 'Conference 1', 'dates': 'June 1-3, 2023', 'location': 'City A'},
+        {"id":1,'name': 'Conference 2', 'dates': 'July 5-7, 2023', 'location': 'City B'},
+        {"id":2,'name': 'Conference 3', 'dates': 'August 10-12, 2023', 'location': 'City C'},
+        {"id":3,'name': 'Conference 4', 'dates': 'September 15-17, 2023', 'location': 'City D'},
+        {"id":4,'name': 'Conference 5', 'dates': 'October 20-22, 2023', 'location': 'City E'},
     ]
     return render(request, 'conference_list.html', {'conferences': conferences})
 
@@ -14,13 +17,43 @@ def conference_create(request):
     return render(request, 'conference_create.html')
 
 def conference_details(request, conference_id):
-    conference = {'name': 'Conference', 'dates': 'June 1-3, 2023', 'location': 'City A', 'topics': 'Topic 1, Topic 2'}
-    return render(request, 'conference_details.html', {'conference': conference})
+    conferences = [
+        {"id":0,'name': 'Conference 1', 'dates': 'June 1-3, 2023', 'location': 'City A'},
+        {"id":1,'name': 'Conference 2', 'dates': 'July 5-7, 2023', 'location': 'City B'},
+        {"id":2,'name': 'Conference 3', 'dates': 'August 10-12, 2023', 'location': 'City C'},
+        {"id":3,'name': 'Conference 4', 'dates': 'September 15-17, 2023', 'location': 'City D'},
+        {"id":4,'name': 'Conference 5', 'dates': 'October 20-22, 2023', 'location': 'City E'},
+    ]
+
+    for item in conferences:
+         if (item["id"] == conference_id):
+            selectedData = item
+    return render(request, 'conference_details.html', {'conference': selectedData})
 
 def conference_update(request, conference_id):
-    conference = {'name': 'Conference', 'dates': 'June 1-3, 2023', 'location': 'City A', 'topics': 'Topic 1, Topic 2'}
-    return render(request, 'conference_update.html', {'conference': conference})
+    conferences = [
+        {"id":0,'name': 'Conference 1', 'dates': 'June 1-3, 2023', 'location': 'City A'},
+        {"id":1,'name': 'Conference 2', 'dates': 'July 5-7, 2023', 'location': 'City B'},
+        {"id":2,'name': 'Conference 3', 'dates': 'August 10-12, 2023', 'location': 'City C'},
+        {"id":3,'name': 'Conference 4', 'dates': 'September 15-17, 2023', 'location': 'City D'},
+        {"id":4,'name': 'Conference 5', 'dates': 'October 20-22, 2023', 'location': 'City E'},
+    ]
+
+    for item in conferences:
+         if (item["id"] == conference_id):
+            selectedData = item
+    return render(request, 'conference_update.html', {'conference': selectedData})
 
 def conference_delete(request, conference_id):
-    conference = {'name': 'Conference', 'dates': 'June 1-3, 2023', 'location': 'City A', 'topics': 'Topic 1, Topic 2'}
-    return render(request, 'conference_delete.html', {'conference': conference})
+    conferences = [
+        {"id":0,'name': 'Conference 1', 'dates': 'June 1-3, 2023', 'location': 'City A'},
+        {"id":1,'name': 'Conference 2', 'dates': 'July 5-7, 2023', 'location': 'City B'},
+        {"id":2,'name': 'Conference 3', 'dates': 'August 10-12, 2023', 'location': 'City C'},
+        {"id":3,'name': 'Conference 4', 'dates': 'September 15-17, 2023', 'location': 'City D'},
+        {"id":4,'name': 'Conference 5', 'dates': 'October 20-22, 2023', 'location': 'City E'},
+    ]
+
+    for item in conferences:
+         if (item["id"] == conference_id):
+            selectedData = item
+    return render(request, 'conference_delete.html', {'conference': selectedData})￼Enter
