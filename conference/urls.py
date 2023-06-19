e4464c66395ce168420c25ea046066e06163178f
ashimwedenyse <denyseashimwe@gmail.com>
2023-06-19 18:14:40 +0200

    update conference details

--- conference/urls.py
+++ conference/urls.py
@@ -5,6 +5,7 @@ from conference import views
 urlpatterns = [
     path('', views.listconference, name='view_conferences'),
     path('create/', views.create_conference, name='create'),
-    path('<str:id>/',views.listconferenceId)
+    path('<str:id>/',views.listconferenceId),
+    path('<str:id>/update',views.updateconference),
 
 ]
--- conference/views.py
+++ conference/views.py
@@ -21,4 +21,13 @@ def listconferenceId(request,id):
                 }
     return render(request, 'view_conferenceId.html',{'conference':conferences,'id':id})
 
+def updateconference(request,id):
+    conferences={'1':{'name':'Social Affairs','date':'1/1/2025','location':'Lectures room'},
+                '2':{'name':'Education','date':'2/1/2025','location':'Muhabura'},
+                '3':{'name':'E-Health','date':'3/1/2025','location':'Kiyovu'},
+                '4':{'name':'Public Talk','date':'4/1/2025','location':'Nyarugenge'},
+                '5':{'name':'Entertainment','date':'5/1/2025','location':'Arena'}
+                }
+    return render(request, 'update.html',{'conference':conferences,'id':id})
+
 
