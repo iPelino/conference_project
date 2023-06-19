d94b919134a316021c1f8571a6dce8fdacc7c733
manzidavidrw <manzi2020d@gmail.com>
2023-06-19 23:59:43 +0200

    second commit

--- conference_project/settings.py
+++ conference_project/settings.py
@@ -1,12 +1,1 @@
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
-    'conference',
-]
-
