d90b241772f6b2e4ec55b7556bf764f7871df7a8
Urikoraho <utikoraho@gmail.com>
2023-06-19 23:40:11 +0200

    list speaker

---
+++ speaker/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from speaker import views
+urlpatterns = [
+    # conference
+    path('speakers/', views.speakers_view,name='speakers'),
+    path('speakers/create', views.create,name='createspeaker'),
+    path('speakers/<id>', views.speaker,name='speaker'),
+
+]
