e27e10ba7c6d7f7f0038835b9b120fdfb2f14e72
Bruno <84916998+Brunoish@users.noreply.github.com>
2023-06-19 10:27:37 +0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,20 +1,26 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+def conference_list(request):
+    conferences = [
+        {'name': 'Conference 1', 'dates': 'June 1-3, 2023', 'location': 'City A'},
+        {'name': 'Conference 2', 'dates': 'July 5-7, 2023', 'location': 'City B'},
+        {'name': 'Conference 3', 'dates': 'August 10-12, 2023', 'location': 'City C'},
+        {'name': 'Conference 4', 'dates': 'September 15-17, 2023', 'location': 'City D'},
+        {'name': 'Conference 5', 'dates': 'October 20-22, 2023', 'location': 'City E'},
+    ]
+    return render(request, 'conference/conference_list.html', {'conferences': conferences})
 
-def home_view(request):
-    data = ['django', 'laravel', 'asp.net core', 'express']
-    return render(request, 'home.html', {'data': data})
+def conference_create(request):
+    return render(request, 'conference/conference_create.html')
 
+def conference_details(request, conference_id):
+    conference = {'name': 'Conference', 'dates': 'June 1-3, 2023', 'location': 'City A', 'topics': 'Topic 1, Topic 2'}
+    return render(request, 'conference/conference_details.html', {'conference': conference})
 
-def about_view(request):
-    return HttpResponse("<h1>About Page</h1><br><a href='/'>Go back to home</a>")
+def conference_update(request, conference_id):
+    conference = {'name': 'Conference', 'dates': 'June 1-3, 2023', 'location': 'City A', 'topics': 'Topic 1, Topic 2'}
+    return render(request, 'conference/conference_update.html', {'conference': conference})
 
-
-def testing_stuff(request, number):
-    # DB query
-    #
-    # number = ''
-    # if id < 5:
-    #     number = 'xzy'
-    return render(request, 'testing.html', {'number': number})
+def conference_delete(request, conference_id):
+    conference = {'name': 'Conference', 'dates': 'June 1-3, 2023', 'location': 'City A', 'topics': 'Topic 1, Topic 2'}
+    return render(request, 'conference/conference_delete.html', {'conference': conference})
