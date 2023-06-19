18bad32d239ddca9bcb0e1a16732590b10159d9e
umulisa11 <angeliqueumulisa8@gmail.com>
2023-06-19 22:57:18 +0200

    conferences

---
+++ conference/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from conference import views
+
+urlpatterns = [
+    path('conferences/', views.conferences_view),
+]
+
+   
---
+++ conference/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+conferences = [
+        {
+            "id": 1,
+            "name": "Umushyikirino",
+            "date": "2023-03-12",
+            "location": "kigali convention center"
+        },
+        {
+            "id": 2,
+            "name": "CHOGMA",
+            "date": "2023-12-12",
+            "location": "Bk arena"
+        },
+        {
+            "id": 3,
+            "name": "Ndaba event",
+            "date": "2023-04-02",
+            "location": "mille colline"
+        },
+
+        {
+            "id": 4,
+            "name": "Mbaza nlp",
+            "date": "2024-01-01",
+            "location": "kivu noir"
+        },
+
+        {
+            "id": 5,
+            "name": "",
+            "date": "2024-07-01",
+            "location": "BK Arena"
+        },
+    ]
+def conferences_view(request):
+    return render(request, 'conference/conferences.html', context={"conferences": conferences})
+
+
+
