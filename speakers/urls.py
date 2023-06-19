c36e9a4a6c67b551f506eb53ef3d32228409046c
Jovz19200 <gisubizo.jovan@gmail.com>
2023-06-19 20:57:41 +0200

    add speaker id to speaker info feature

--- speakers/urls.py
+++ speakers/urls.py
@@ -3,4 +3,6 @@ from django.urls import  path, include
 
 urlpatterns = [
     path('/', views.view_speakers, name = 'view speakers'),
+    path('create_speaker/', views.create_speaker),
+    path('<speaker_id: speakers.id>/', views.speaker_detail)
 ]
