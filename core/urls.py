5ab2abfe05846d7fc63cb54cea1424c04b4b7346
nancykirezi <wanancykirezi@gmail.com>
2023-06-19 09:39:07 -0400

    first question of list conferences using html and django

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,12 @@ from core import views
 urlpatterns = [
     # www.conference.rw
     path('', views.home_view, name='home'),
+    path('conferences/',views.all_conference_view,name='conference'),
     path('<str:number>/', views.testing_stuff, name='testing'),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
 
     # www.conference.rw/about/
     path('about/', views.about_view, name='about'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,14 @@ def home_view(request):
     data = ['django', 'laravel', 'asp.net core', 'express']
     return render(request, 'home.html', {'data': data})
 
+def conference_list(request):
+    data=[
+    {"id": 1.0,'name': 'Conference A','dates': '12th may 2023','location': 'Area A'},
+    {"id": 1.1,'name': 'Conference B','dates': '1st January 1990 to 5th  February 1995.','location': 'Area B'},
+    {"id": 1.2,'name': 'Conference C','dates': '23rd January 1999 to 5th  March 19999','location': 'Area C'},
+    {"id": 1.3,'name': 'Conference D','dates': '1st January 2000 to 5th  February 2000','location': 'Area D'},
+    {"id": 1.4,'name': 'Conference E','dates': '21st January 20223 to 25th January 2023','location': 'Area E'}]
+    return render(request,'conference.html',{"data":data})
 
 def about_view(request):
     return HttpResponse("<h1>About Page</h1><br><a href='/'>Go back to home</a>")
