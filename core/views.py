fc9a5c0bb053e9f8d63abac195304f2cdf90d9d7
Henry Shema <84914138+shemahenry@users.noreply.github.com>
2023-06-19 22:41:24 +0200

    solve issue 1

--- core/views.py
+++ core/views.py
@@ -1,6 +1,43 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def conference_list(request):
+    conferences = [
+        {
+            'id': 0,
+            'name': 'Conference A',
+            'dates': 'June 1-3, 2023',
+            'location': 'City V',
+        },
+        {
+            'id': 1,
+            'name': 'Conference B',
+            'dates': 'July 10-12, 2023',
+            'location': 'City W',
+        },
+        {
+            'id': 2,
+            'name': 'Conference C',
+            'dates': 'August 5-7, 2023',
+            'location': 'City X',
+        },
+        {
+            'id': 3,
+            'name': 'Conference D',
+            'dates': 'August 5-7, 2023',
+            'location': 'City Y',
+        },
+        {
+            'id': 4,
+            'name': 'Conference E',
+            'dates': 'August 5-7, 2023',
+            'location': 'City Z',
+        },
+    ]
+    context = {
+        'conferences': conferences
+    }
+    return render(request, 'conference/conference_list.html', context)
 
 def home_view(request):
     data = ['django', 'laravel', 'asp.net core', 'express']
