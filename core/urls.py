5040660229a72a0b9e0fcdf4e66291d0bfe4c8e6
nancykirezi <wanancykirezi@gmail.com>
2023-06-19 09:59:05 -0400

    step two of creating  a conference using html. we didn't take in count the backend

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     # www.conference.rw
     path('', views.home_view, name='home'),
     path('conferences/',views.all_conference_view,name='conference'),
+      path('conferences/create/',views.create_conference,name='create'),
     path('<str:number>/', views.testing_stuff, name='testing'),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
--- core/views.py
+++ core/views.py
@@ -15,6 +15,25 @@ def conference_list(request):
     {"id": 1.4,'name': 'Conference E','dates': '21st January 20223 to 25th January 2023','location': 'Area E'}]
     return render(request,'conference.html',{"data":data})
 
+def create_conference(request):
+    return render(request, 'create.html')
+
+def conference_detail(request,conference_id):
+    data=[
+    {"id": 1.0,'name': 'Conference A','dates': '12th may 2023','location': 'Area A'},
+    {"id": 1.1,'name': 'Conference B','dates': '1st January 1990 to 5th  February 1995.','location': 'Area B'},
+    {"id": 1.2,'name': 'Conference C','dates': '23rd January 1999 to 5th  March 19999','location': 'Area C'},
+    {"id": 1.3,'name': 'Conference D','dates': '1st January 2000 to 5th  February 2000','location': 'Area D'},
+    {"id": 1.4,'name': 'Conference E','dates': '21st January 20223 to 25th January 2023','location': 'Area E'}]
+# searching for a specific data
+    for item in data:
+        if (item["id"] == conference_id):
+            selectedData = item
+    return render(request, 'detail.html',{"data":selectedData})
+
+
+    
+    
 def about_view(request):
     return HttpResponse("<h1>About Page</h1><br><a href='/'>Go back to home</a>")
 
