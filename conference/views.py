91f40273bc82700e0b2fed07f7aacf187b1cfe3f
umbereye26 <carineumbereye56@gmail.com>
2023-06-19 10:06:47 -0700

    My first commit on conference project

---
+++ conference/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+conferences = [
+        {
+            "id": 1,
+            "name": "Introduction to AI",
+            "date": "2023-03-12",
+            "location": "CcHUB"
+        },
+        {
+            "id": 2,
+            "name": "Gils in ICT",
+            "date": "2023-12-12",
+            "location": "BK Arena"
+        },
+        {
+            "id": 3,
+            "name": "Impact of AI on employment",
+            "date": "2023-04-02",
+            "location": "Lemigo Hotel"
+        },
+
+        {
+            "id": 4,
+            "name": "Developing Tech in Rwanda",
+            "date": "2024-01-01",
+            "location": "Intare Conference Arena"
+        },
+
+        {
+            "id": 5,
+            "name": "Women in Tech",
+            "date": "2024-07-01",
+            "location": "Radisson Hotel"
+        },
+    ]
+def conferences_list(request):
+    return render(request,'dataconference.html' ,context={"conferences": conferences})
+
