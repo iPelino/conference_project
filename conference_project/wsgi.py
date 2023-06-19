768e140ecde332268af354f2da34046a385720f2
Ntwaringererocorneille <84917157+Ntwaringererocorneille@users.noreply.github.com>
2023-06-19 23:13:09 +0200

    Add files via upload

--- conference_project/wsgi.py
+++ conference_project/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.index.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conference_project.settings')
 
