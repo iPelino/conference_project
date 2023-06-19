b362521c2bef601ead27be995e9a932652d6e267
sekuruwilson <sekuruwilson7@gmail.com>
2023-06-19 21:25:40 +0200

    Id listing updates

--- conference/urls.py
+++ conference/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, re_path
 from conference import views
 urlpatterns = [
     path('',views.list_conference,name="lists"),
+    path('create/',views.create_conference),
+    path('<str:id>/',views.Id_conference),
 ]
