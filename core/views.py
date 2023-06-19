e5b5a9108fcb883bf174aebbd9e91c23cd1c2bb5
lilianebahatsi <84917014+lilianebahatsi@users.noreply.github.com>
2023-06-19 22:59:11 +0200

    Add views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Conference
 
+def list_conference(request):
+    data = Conference.objects.all().values()
+    context = { 'my_conference': data }
+    return render(request, 'conference_list.html',context)
+
+def Add_conference(request):
+    return render(request, 'add_conference.html', )
+
+def single_conference(request, conference_id):
+    data = Conference.objects.all().values()
+    context = { 'my_conference': data }
+    return render(request, 'single_conference.html',context)
+    
+def update_conference(request, conference_id):
+    return render(request, 'conference_form.html')
+
+def remove_conference(request, conference_id):
+    items = Conference.objects.all().values()
+    context = { 'my_conference': data }
+    return render(request, 'remove_conference.html', context)
 
 def home_view(request):
     data = ['django', 'laravel', 'asp.net core', 'express']
