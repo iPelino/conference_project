9874ec4340cf9a57c031c9c95cf523b3661d335c
Henry Shema <84914138+shemahenry@users.noreply.github.com>
2023-06-19 22:59:46 +0200

    Add url for viewing conference info

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('<str:number>/', views.testing_stuff, name='testing'),
     path('conferences/', conference_list, name='conference_list'),
     path('conferences/create/', views.create_conference, name='create_conference'),
+    path('conferences/<int:conference_id>/', views.conference_details, name='conference_details'),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
 
