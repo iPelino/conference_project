b8da8c3e1a41839216c8365684402d9c673408b5
lilianebahatsi <84917014+lilianebahatsi@users.noreply.github.com>
2023-06-19 23:08:32 +0200

    add model in database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Conference(models.Model):
+    name = models.CharField(max_length = 255)
+    location = models.CharField(max_length = 255)
+    dates = models.DateField(max_length = 255)
+    topic = models.CharField(max_length = 255)
+
 # Create your models here.
