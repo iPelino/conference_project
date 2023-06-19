e5bc1da42663dadca5542e68a5a8cb09b764f1f2
Merchior <abawemerchior@gmail.com>
2023-06-20 03:36:05 +0600

    This is new change to the way of solving issue1

--- conference/urls.py
+++ conference/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('conferences/',views.list_conferences,name="List of Conferences"),
     path('conferences/create/',views.create_new),
+    path('conferences/create.html',views.create_new),
     path('conferences/create/conferences.html',views.list_conferences),
     path('conferences/create/create.html',views.create_new)
 
