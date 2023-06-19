d8a9db219672ebc0b9a91242652fc1d0f441b618
Umubyeyi Esperance <umuespe@gmail.com>
2023-06-20 00:12:27 +0200

    /conferences/<conference_id>/delete/: Form to delete a specific conference

--- conference/urls.py
+++ conference/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.conference, name='conferences'),
     path('<int:conference_id>/', views.conference_details, name='conference_details'),
-    path('<int:conference_id>/update', views.update_page, name='conference_update_page'),
-    path('create/', views.create_page, name='conference_create_page')
+    path('<int:conference_id>/update', views.conference_update, name='conference_update_page'),
+    path('<int:conference_id>/update/confirm', views.conference_update_confirm, name='conference_update_confirm'),
+    path('<int:conference_id>/delete', views.conference_delete, name='conference_delete'),
+    path('<int:conference_id>/delete/confirm', views.conference_delete_confirm, name='conference_delete_confirm'),
+    path('create/', views.conference_create, name='conference_create_page')
 ]
