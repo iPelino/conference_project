f568dcdfe6b313f4a4a716a0f8a713023dd3f8ce
Epiphanie123 <epiphaniemushimiyimana7@gmail.com>
2023-06-19 19:15:17 -0500

    Implemented 'speakers' app with list, create, detail, update, and delete functionalities for managing conference speakers.

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, re_path
-from core import views
+from speakers import views
+
+
 urlpatterns = [
     # www.conference.rw
     path('', views.home_view, name='home'),
     path('<str:number>/', views.testing_stuff, name='testing'),
+     path('speakers/', include('speakers.urls')),
     # path('<pk:id>/', views.testing_stuff, name='testing'),
     # path('<slug:slug>/', views.testing_stuff, name='testing'),
-
+    path('', views.speaker_list, name='speaker_list'),
+    path('create/', views.speaker_create, name='speaker_create'),
+    path('<int:speaker_id>/', views.speaker_detail, name='speaker_detail'),
+    path('<int:speaker_id>/update/', views.speaker_update, name='speaker_update'),
+    path('<int:speaker_id>/delete/', views.speaker_delete, name='speaker_delete'),
     # www.conference.rw/about/
     path('about/', views.about_view, name='about'),
 ]
