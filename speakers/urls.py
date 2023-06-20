a2e8d54b04cf712ffae70bbef0d9678bce136049
Gratia ishimwe <ishimwegratia22@gmail.com>
2023-06-20 19:19:29 +0200

    add speaker

--- speakers/urls.py
+++ speakers/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from speakers import views
 
 urlpatterns = [
- path('/', views.speaker_list),
- path('/<speaker_id: speakers.id>/',views.speaker_details)
-] 
+    path('/', views.speaker_list)
+    path('create speaker',views.)
+    path('/<speaker_id: speakers.id>/' , views.speaker_details),
+    path('<int:speaker_id>/update/', views.update_speaker, name='update_speaker'),
+    path('<int:speaker_id>/delete/', views.delete_speaker, name='delete_speaker'),
+]
