63e40614e4adbf72f484d3ed8e5e8a7985c65683
ineza-nadine <nadineineza12@gmail.com>
2023-06-19 23:04:35 -0700

    created a view for details of specific conference

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from requests import request
+import conference_project
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,7 +9,7 @@ def home_view(request):
     return render(request, 'home.html', {'data': data})
 
 
-def about_view(request):
+def about_view():
     return HttpResponse("<h1>About Page</h1><br><a href='/'>Go back to home</a>")
 
 
@@ -19,10 +21,18 @@ def testing_stuff(request, number):
     #     number = 'xzy'
     return render(request, 'testing.html', {'number': number})
 
-    def all_my_conferences(request):
-      conferences = Conference.objects.all()  
+def all_conferences(request):
+      conferences = conference_project.objects.all()  
       return render(request, 'conference/conferences.html', {'conferences': conferences})
     
-    def new_conference(request):
+def new_conference(request):
         return render(request, 'conference/new_conference.html')
+
+def conference_details(request, conference_id):
+       conference = conference.objects.get(id=conference_id)
+       return render(request, 'conference/conference_detail.html', {'conference': conference})
+     
+          
+
+
  
