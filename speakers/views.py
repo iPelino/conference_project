65f649ba79213112c10a9f552d476e2699158993
mukamusoni1 <136576833+mukamusoni1@users.noreply.github.com>
2023-06-20 20:48:42 +0000

    added update speaker func

--- speakers/views.py
+++ speakers/views.py
@@ -13,4 +13,6 @@ def speaker_list(request):
 def create_speaker(request):
     return render(request, 'speakers/create_speaker.html')
 def speaker_details(request, speaker_id):
-    return render(request), 'speakers/speaker_details.html', {'speakers': speakers}
+    return render(request, 'speakers/speaker_details.html', {'speakers': speakers})
+def update_speaker(request, speaker_id):
+    return render(request, 'speakers/update_speaker.html', {'speakers': speakers})
